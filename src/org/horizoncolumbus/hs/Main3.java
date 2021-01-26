93384f173adb90591b059d4dc50ff80cbad8d048
Tyler Fowsantia <student.t.fowsantia@horizoncolumbus.org>
2021-01-26 09:12:43 -0500

    fixed sum array #3

--- src/org/horizoncolumbus/hs/Main3.java
+++ src/org/horizoncolumbus/hs/Main3.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Main3 {
 
     public static void main(String[] args) {
-        int sum = 0;
-        Scanner arraySize = new Scanner(System.in);
+        Scanner scan1 = new Scanner(System.in);
         System.out.println("Enter array size: ");
 
-        String size = arraySize.nextLine(); int intSize = Integer.parseInt(size);
+        int intSize = scan1.nextInt();
+        int[] numbers3 = new int[intSize];
         for(int x = 0; x < intSize; x++) {
-            if(x < intSize) {
-                Scanner value = new Scanner(System.in);
-                System.out.println("Enter array value: ");
-                String getValue = value.nextLine(); int intValue = Integer.parseInt(getValue);
-                sum += intValue;
-            }
+            System.out.println("Enter value[" + x + "]: ");
+            numbers3[x] = scan1.nextInt();
+        }
+        int sum = 0;
+        for(int x = 0; x < intSize; x++) {
+            sum = sum + numbers3[x];
         }
         System.out.println("The sum value is: " + sum);
     }
