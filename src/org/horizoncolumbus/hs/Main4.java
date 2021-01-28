96699343fb6bc6986509122076ac007c4c84cd45
Tyler Fowsantia <student.t.fowsantia@horizoncolumbus.org>
2021-01-28 09:19:16 -0500

    completed sum array #4

---
+++ src/org/horizoncolumbus/hs/Main4.java
@@ -0,0 +1,22 @@
+package org.horizoncolumbus.hs;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+// 1/28/2021
+
+public class Main4 {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        String[] pog = new String[3];
+        pog[0] = "https://classroom.google.com/u/0/h";
+        pog[1] = "https://zoom.us/signin";
+        pog[2] = "https://www.youtube.com/watch?v=s49CT4DTAkw";
+
+        for(int i = 0; i < pog.length; i++) {
+            Desktop.getDesktop().browse(new URI(pog[i]));
+        }
+    }
+}
